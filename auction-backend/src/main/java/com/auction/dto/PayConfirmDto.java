cdb942a4d87cc96224f232b66f495bba0b16c522
confxer <swiri01999257@gmail.com>
2025-08-12 10:54:08 +0900

    rrr

--- auction-backend/src/main/java/com/auction/dto/PayConfirmDto.java
+++ auction-backend/src/main/java/com/auction/dto/PayConfirmDto.java
@@ -2,9 +2,11 @@ package com.auction.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PayConfirmDto {
     private String paymentKey;
     private String orderId;
--- auction-backend/src/main/java/com/auction/dto/PayValidDto.java
+++ auction-backend/src/main/java/com/auction/dto/PayValidDto.java
@@ -2,9 +2,11 @@ package com.auction.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PayValidDto {
     private String orderId;
     private Long amount;
