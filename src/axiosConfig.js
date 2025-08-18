a226268f622dd7367c78b6163a5dbc25786d40d6
confxer <swiri01999257@gmail.com>
2025-08-18 15:11:01 +0900

    ㅇ

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAccessToken, getRefreshToken, clearTokens } from './utils/cookieUtils';
 
 // axios 기본 설정
-axios.defaults.baseURL = "auction-alb-925869368.ap-northeast-2.elb.amazonaws.com";
+axios.defaults.baseURL = "http://auction-alb-925869368.ap-northeast-2.elb.amazonaws.com";
 axios.defaults.withCredentials = true; // 쿠키 자동 전송
 
 // 요청 인터셉터 - 토큰 자동 추가
