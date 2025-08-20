327f2896914854788f294909d0f82ed4e4266753
confxer <swiri01999257@gmail.com>
2025-08-20 10:32:46 +0900

    시발이건아니잖아

--- auction-backend/src/main/java/com/auction/service/EmailService.java
+++ auction-backend/src/main/java/com/auction/service/EmailService.java
@@ -48,9 +48,7 @@ public class EmailService {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject("[경매 시스템] 이메일 인증");
-        message.setText("안녕하세요!\n\n" +
-                "경매 시스템 회원가입을 완료하려면 아래 링크를 클릭해주세요:\n\n" +
-                "http://auction-react-bucket-20250804-prj.s3-website.ap-northeast-2.amazonaws.com/verify-email?token=" + token + "\n\n" +
+        message.setText("안녕하세요!\n\n" + token + "\n\n" +
                 "이 링크는 24시간 후에 만료됩니다.\n\n" +
                 "감사합니다.");
         
@@ -74,8 +72,7 @@ public class EmailService {
         message.setSubject("[경매 시스템] 비밀번호 재설정");
         message.setText("안녕하세요!\n\n" +
                 "비밀번호 재설정을 요청하셨습니다.\n\n" +
-                "아래 링크를 클릭하여 새 비밀번호를 설정해주세요:\n\n" +
-                "http://auction-react-bucket-20250804-prj.s3-website.ap-northeast-2.amazonaws.com/reset-password?token=" + token + "\n\n" +
+                 token + "\n\n" +
                 "이 링크는 1시간 후에 만료됩니다.\n\n" +
                 "요청하지 않으셨다면 이 메일을 무시하셔도 됩니다.\n\n" +
                 "감사합니다.");
